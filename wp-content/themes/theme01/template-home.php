daebe0a860647c3397140cf5fbc5a58a76ba0b61
Gokarna Chaudhay <gokarnachy28@gmail.com>
2024-11-06 14:55:29 +0545

    wp_query, custom_post_type movie, custom_taxonomy,

--- wp-content/themes/theme01/template-home.php
+++ wp-content/themes/theme01/template-home.php
@@ -6,24 +6,54 @@ get_header();// call header
 
 <?php
 //returns the category 
-$category = get_categories(array('taxonomy' => 'category'));
+//$category = get_categories(array('taxonomy' => 'category'));
 // echo "<pre>";
 // print_r($category);
 // echo "</pre>";
 ?>
+<!-- <div class="category-container">
+    <?php
+    // foreach ($category as $categoryValue) {
+    ?>
+        <a class="category-card" href="<? php// echo get_category_link($categoryValue->term_id); ?>">
+            <div>
+                <p><?php //echo $categoryValue->name ?><span>(<?php //echo $categoryValue->count ?>)</span></p>
+            </div>
+        </a>
+
+        <?php
+        // }
+        ?>
+</div> -->
+
+<!-- custom taxonomy -->
+ <div class="category_wrapper">
+<?php
+$movieCate = get_terms(['taxonomy' => 'movies_category', 'hide_empty' => false]);
+// echo "<pre>";
+// print_r($movieCate);
+// echo "<pre>";
+?>
 <div class="category-container">
     <?php
-    foreach ($category as $categoryValue) {
+    foreach ($movieCate as $movie_cate) {
         ?>
-        <a class="category-card" href="<?php echo get_category_link($categoryValue->term_id); ?>">
+        <a class="category-card" href="<?php echo get_category_link($movie_cate->term_id); ?>">
             <div>
-                <p><?php echo $categoryValue->name ?><span>(<?php echo $categoryValue->count ?>)</span></p>
+                <p><?php echo $movie_cate->name ?></p>
             </div>
         </a>
 
         <?php
     }
     ?>
+</div>
+</div>
+
+<!-- latest movies section  -->
+<div class="movie_wrapper">
+    <h2>Latest Movies</h2>
+    <?php get_template_part('template-parts/movies/latest_movies') ?>
 </div>
 <?php
 get_footer(); // call footer
---
+++ wp-content/themes/theme01/template-parts/movies/latest_movies.php
@@ -0,0 +1,31 @@
+<div class="movie_card_container">
+
+    <?php
+    $wpmovie = array(
+        'post_type' => 'movies',
+        'post_status' => 'publish',
+    );
+
+    $moviesQuery = new Wp_Query($wpmovie);
+
+    while ($moviesQuery->have_posts()) {
+        $moviesQuery->the_post();
+
+        ?>
+        <a href="" class="movie_card">
+            <div class="movie_image">
+                <?php
+                $image_path = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail');
+                ?>
+                <img src="<?php echo $image_path[0]; ?>" alt="movie image">
+            </div>
+            <div class="movie_details">
+                <p class="movie_title"><?php the_title(); ?></p>
+                <p class="movie_date"><?php echo get_the_date(); ?></p>
+            </div>
+        </a>
+
+        <?php
+    }
+    ?>
+</div>
